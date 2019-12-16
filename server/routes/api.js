3f35f0107bd06107e00f957c0e12fa63e151d29a
Michelangelo Marani <michelangelomarani88@gmail.com>
2019-12-16 22:54:13 +0100

    fixed some error in api

--- server/routes/api.js
+++ server/routes/api.js
@@ -9,7 +9,7 @@ const User = require('../models').User;
 /*--- Version ---*/
 
 // GET /api/version
-router.get('/version', function (req, res) {
+router.get('/version', function(req, res) {
     res.send({ version: 'v0.2.0' });
 });
 
@@ -20,13 +20,13 @@ router.get('/version', function (req, res) {
 // get help list
 router.get('/helps', (req, res) =>
     models.Help.findAll()
-        .then(helps => {
-            res.json(helps)
-        })
-        .catch(err => {
-            console.log(err);
-            res.sendStatus(500)
-        })
+    .then(helps => {
+        res.json(helps)
+    })
+    .catch(err => {
+        console.log(err);
+        res.sendStatus(500)
+    })
 );
 
 // add an help
@@ -50,14 +50,14 @@ router.post('/helps/add', (req, res) => {
 /*--- Users ---*/
 
 // POST /api/user
-router.post('/user', function (req, res) {
+router.post('/user', function(req, res) {
     User.create({ firstName: "Jane", lastName: "Doe" }).then(user => {
         res.send({ id: user.id });
     });
 });
 
 // DELETE /api/user
-router.delete('/user', function (req, res) {
+router.delete('/user', function(req, res) {
     User.destroy({
         where: {
             firstName: "Jane"
@@ -68,7 +68,7 @@ router.delete('/user', function (req, res) {
 });
 
 // PUT /api/user
-router.put('/user', function (req, res) {
+router.put('/user', function(req, res) {
     User.update({ lastName: "Doe" }, {
         where: {
             lastName: null
@@ -83,7 +83,7 @@ router.put('/user', function (req, res) {
 
 // Signup
 
-router.post('/signup', function (req, res) {
+router.post('/signup', function(req, res) {
     console.log(req.body);
     if (!req.body.username || !req.body.password) {
         res.status(400).send({ msg: 'Please pass usernamer and password.' })
@@ -103,9 +103,9 @@ router.post('/signup', function (req, res) {
 
 // Signin
 
-router.post('/signin', function (req, res) {
+router.post('/signin', function(req, res) {
     User
-        .find({
+        .findOne({
             where: {
                 username: req.body.username
             }
@@ -119,7 +119,7 @@ router.post('/signin', function (req, res) {
             user.comparePassword(req.body.password, (err, isMatch) => {
                 if (isMatch && !err) {
                     var token = jwt.sign(JSON.parse(JSON.stringify(user)), '***REMOVED-JWT-SECRET***', { expiresIn: 86400 * 30 });
-                    jwt.verify(token, '***REMOVED-JWT-SECRET***', function (err, data) {
+                    jwt.verify(token, '***REMOVED-JWT-SECRET***', function(err, data) {
                         console.log(err, data);
                     })
                     res.json({ success: true, token: 'JWT ' + token });
@@ -128,11 +128,14 @@ router.post('/signin', function (req, res) {
                 }
             })
         })
-        .catch((error) => res.status(400).send(error));
+        .catch((error) => {
+            console.log(error)
+            res.status(400).send(error)
+        });
 });
 
 // Function to extract token
-getToken = function (headers) {
+getToken = function(headers) {
     if (headers && headers.authorization) {
         var parted = headers.authorization.split(' ');
         if (parted.length === 2) {

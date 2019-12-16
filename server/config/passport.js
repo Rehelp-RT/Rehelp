7181975028d0bde949c94042de0a6e44b746f8f5
Edoardo <edoardopiergentili@gmail.com>
2019-12-16 17:02:33 +0100

    api signin e signup

---
+++ server/config/passport.js
@@ -0,0 +1,18 @@
+const JwtStrategy = require('passport-jwt').Strategy,
+    ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// load up the user model
+const User = require('../models').User;
+
+module.exports = function (passport) {
+    const opts = {
+        jwtFromRequest: ExtractJwt.fromAuthHeaderWithScheme('JWT'),
+        secretOrKey: '***REMOVED-JWT-SECRET***',
+    };
+    passport.use('jwt', new JwtStrategy(opts, function (jwt_paylod, done) {
+        User
+            .findByPk(jwt_paylod.id)
+            .then((user) => { return done(null, user); })
+            .catch((error) => { return done(error, false); });
+    }));
+};
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,11 +1,15 @@
 const express = require('express');
-const models = require('../models');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
 const router = express.Router();
+require('../config/passport')(passport);
+const models = require('../models');
+const User = require('../models').User;
 
 /*--- Version ---*/
 
 // GET /api/version
-router.get('/version', function(req, res) {
+router.get('/version', function (req, res) {
     res.send({ version: 'v0.2.0' });
 });
 
@@ -16,13 +20,13 @@ router.get('/version', function(req, res) {
 // get help list
 router.get('/helps', (req, res) =>
     models.Help.findAll()
-    .then(helps => {
-        res.json(helps)
-    })
-    .catch(err => {
-        console.log(err);
-        res.sendStatus(500)
-    })
+        .then(helps => {
+            res.json(helps)
+        })
+        .catch(err => {
+            console.log(err);
+            res.sendStatus(500)
+        })
 );
 
 // add an help
@@ -46,14 +50,14 @@ router.post('/helps/add', (req, res) => {
 /*--- Users ---*/
 
 // POST /api/user
-router.post('/user', function(req, res) {
+router.post('/user', function (req, res) {
     User.create({ firstName: "Jane", lastName: "Doe" }).then(user => {
         res.send({ id: user.id });
     });
 });
 
 // DELETE /api/user
-router.delete('/user', function(req, res) {
+router.delete('/user', function (req, res) {
     User.destroy({
         where: {
             firstName: "Jane"
@@ -64,7 +68,7 @@ router.delete('/user', function(req, res) {
 });
 
 // PUT /api/user
-router.put('/user', function(req, res) {
+router.put('/user', function (req, res) {
     User.update({ lastName: "Doe" }, {
         where: {
             lastName: null
@@ -74,6 +78,72 @@ router.put('/user', function(req, res) {
     });
 });
 
+
+/*--- Authentication ---*/
+
+// Signup
+
+router.post('/signup', function (req, res) {
+    console.log(req.body);
+    if (!req.body.username || !req.body.password) {
+        res.status(400).send({ msg: 'Please pass usernamer and password.' })
+    } else {
+        User
+            .create({
+                username: req.body.username,
+                password: req.body.password
+            })
+            .then((user) => res.status(201).send(user))
+            .catch((error) => {
+                console.log(error);
+                res.status(400).send(error);
+            });
+    }
+});
+
+// Signin
+
+router.post('/signin', function (req, res) {
+    User
+        .find({
+            where: {
+                username: req.body.username
+            }
+        })
+        .then((user) => {
+            if (!user) {
+                return res.status(401).send({
+                    message: 'Authentication failed. User not found.',
+                });
+            }
+            user.comparePassword(req.body.password, (err, isMatch) => {
+                if (isMatch && !err) {
+                    var token = jwt.sign(JSON.parse(JSON.stringify(user)), '***REMOVED-JWT-SECRET***', { expiresIn: 86400 * 30 });
+                    jwt.verify(token, '***REMOVED-JWT-SECRET***', function (err, data) {
+                        console.log(err, data);
+                    })
+                    res.json({ success: true, token: 'JWT ' + token });
+                } else {
+                    res.status(401).send({ success: false, msg: 'Authentication failed. Wrongg password.' });
+                }
+            })
+        })
+        .catch((error) => res.status(400).send(error));
+});
+
+// Function to extract token
+getToken = function (headers) {
+    if (headers && headers.authorization) {
+        var parted = headers.authorization.split(' ');
+        if (parted.length === 2) {
+            return parted[1];
+        } else {
+            return null;
+        }
+    } else {
+        return null;
+    }
+};
 
 
 // exports

4e142837486d8e96afba1b02f2903f15c78816ad
Michelangelo Marani <michelangelomarani88@gmail.com>
2019-12-16 19:45:15 +0100

    asd

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,14 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../_models';
+import { AuthenticationService } from '../_services';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html'
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent {
+  currentUser: User;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+      private router: Router,
+      private authenticationService: AuthenticationService
+  ) {
+      this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
   }
 
+  logout() {
+      this.authenticationService.logout();
+      this.router.navigate(['/login']);
+  }
 }

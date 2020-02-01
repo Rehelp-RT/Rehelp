526674a6d3fef493f1e071909ad71d74ad34c1d6
Michelangelo Marani <michelangelomarani88@gmail.com>
2020-02-01 15:04:40 +0100

    seed working

--- server/seeders/20200127184112-populate-categories.js
+++ server/seeders/20200127184112-populate-categories.js
@@ -2,22 +2,23 @@
 
 module.exports = {
     up: (queryInterface, Sequelize) => {
+        const date = new Date();
         return queryInterface.bulkInsert('HelpCategories', [
-            { code: 'BAB', name: 'Baby sitting e dog sitting' },
-            { code: 'BES', name: 'Bestiame' },
-            { code: 'CON', name: 'Condivisione della conoscenza' },
-            { code: 'CUC', name: 'Cucina' },
-            { code: 'AGR', name: 'Lavori agricoli' },
-            { code: 'DOM', name: 'Lavori domestici' },
-            { code: 'GDT', name: 'Carte, giochi da tavolo e videogiochi' },
-            { code: 'HOB', name: 'Hobbistica' },
-            { code: 'INF', name: 'Informatica ed elettronica' },
-            { code: 'MUS', name: 'Musica e canto' },
-            { code: 'SOS', name: 'Sostegno alla persona' },
-            { code: 'SPO', name: 'Sport' },
-            { code: 'TRA', name: 'Traslochi' },
-            { code: 'VIA', name: 'Mezzi di trasporto e viaggi' },
-            { code: 'VIT', name: 'Vitto e alloggio' }
+            { code: 'BAB', createdAt: date, updatedAt: date, name: 'Baby sitting e dog sitting' },
+            { code: 'BES', createdAt: date, updatedAt: date, name: 'Bestiame' },
+            { code: 'CON', createdAt: date, updatedAt: date, name: 'Condivisione della conoscenza' },
+            { code: 'CUC', createdAt: date, updatedAt: date, name: 'Cucina' },
+            { code: 'AGR', createdAt: date, updatedAt: date, name: 'Lavori agricoli' },
+            { code: 'DOM', createdAt: date, updatedAt: date, name: 'Lavori domestici' },
+            { code: 'GDT', createdAt: date, updatedAt: date, name: 'Carte, giochi da tavolo e videogiochi' },
+            { code: 'HOB', createdAt: date, updatedAt: date, name: 'Hobbistica' },
+            { code: 'INF', createdAt: date, updatedAt: date, name: 'Informatica ed elettronica' },
+            { code: 'MUS', createdAt: date, updatedAt: date, name: 'Musica e canto' },
+            { code: 'SOS', createdAt: date, updatedAt: date, name: 'Sostegno alla persona' },
+            { code: 'SPO', createdAt: date, updatedAt: date, name: 'Sport' },
+            { code: 'TRA', createdAt: date, updatedAt: date, name: 'Traslochi' },
+            { code: 'VIA', createdAt: date, updatedAt: date, name: 'Mezzi di trasporto e viaggi' },
+            { code: 'VIT', createdAt: date, updatedAt: date, name: 'Vitto e alloggio' }
         ], {});
     },
 

003c61fe4419c4930521137886f7cacfc720612c
Michelangelo Marani <michelangelomarani88@gmail.com>
2020-02-03 17:14:28 +0100

    seeder types with name

--- server/seeders/20200203101705-populate-types.js
+++ server/seeders/20200203101705-populate-types.js
@@ -4,12 +4,12 @@ module.exports = {
     up: (queryInterface, Sequelize) => {
         const date = new Date();
         return queryInterface.bulkInsert('HelpTypes', [
-            { id: 1, code: 'MEH', createdAt: date, updatedAt: date, description: 'Incontra e aiuta (Meet and Help)' },
-            { id: 2, code: 'COH', createdAt: date, updatedAt: date, description: 'Aiuto collettivo (Collaborative Help)' },
-            { id: 3, code: 'IMH', createdAt: date, updatedAt: date, description: 'Aiuto immediato (Immediate Help)' },
-            { id: 4, code: 'MEU', createdAt: date, updatedAt: date, description: 'Parliamone insieme (Meet Up)' },
-            { id: 5, code: 'MES', createdAt: date, updatedAt: date, description: 'Incontra e condividi (Meet and Share)' },
-            { id: 6, code: 'GAS', createdAt: date, updatedAt: date, description: 'Condividi orto (Garden Share)' }
+            { id: 1, code: 'MEH', createdAt: date, updatedAt: date, name: 'Incontra e aiuta (Meet and Help)' },
+            { id: 2, code: 'COH', createdAt: date, updatedAt: date, name: 'Aiuto collettivo (Collaborative Help)' },
+            { id: 3, code: 'IMH', createdAt: date, updatedAt: date, name: 'Aiuto immediato (Immediate Help)' },
+            { id: 4, code: 'MEU', createdAt: date, updatedAt: date, name: 'Parliamone insieme (Meet Up)' },
+            { id: 5, code: 'MES', createdAt: date, updatedAt: date, name: 'Incontra e condividi (Meet and Share)' },
+            { id: 6, code: 'GAS', createdAt: date, updatedAt: date, name: 'Condividi orto (Garden Share)' }
         ], {});
     },
 

c87f15a1151136f7c571a060b21415618046c3e3
Danilo Paoletti <danilopaoletti91@gmail.com>
2020-02-03 23:34:58 +0100

    commit modifiche dettagli

--- src/app/helps-detail/helps-detail.component.ts
+++ src/app/helps-detail/helps-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { HelpService } from '../_services';
+import { Help } from '../_models';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-helps-detail',
@@ -7,9 +10,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelpsDetailComponent implements OnInit {
 
-  constructor() { }
+  help: Help;
+  helpId: number;
+
+  constructor(private hs: HelpService,
+    private actRoute: ActivatedRoute) { }
   
-  ngOnInit() {    
+  ngOnInit() {  
+    this.helpId = this.actRoute.snapshot.params['id'];
+    this.getHelp();  
   }
 
+  getHelp(): void {
+    this.hs.getById(this.helpId)
+        .subscribe(x => {
+            this.help = x;
+        });
+  }
 }

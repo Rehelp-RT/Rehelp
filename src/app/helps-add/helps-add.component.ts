bf2c601620ca18853790683af3f036726cccb310
Michelangelo Marani <michelangelomarani88@gmail.com>
2020-02-10 23:34:51 +0100

    working map

--- src/app/helps-add/helps-add.component.ts
+++ src/app/helps-add/helps-add.component.ts
@@ -4,33 +4,31 @@ import {
   ElementRef,
   NgZone,
   OnInit
-} from "@angular/core";
+} from '@angular/core';
 import {
   AuthenticationService,
   CategoryService,
   HelpService
-} from "@app/_services";
-import { Router } from "@angular/router";
-import { HelpCategory, Help, User } from "@app/_models";
-import { MapsAPILoader, MouseEvent } from "@agm/core";
+} from '@app/_services';
+import { Router } from '@angular/router';
+import { HelpCategory, Help, User } from '@app/_models';
+import { MapsAPILoader, MouseEvent } from '@agm/core';
 
 @Component({
-  selector: "app-helps-add",
-  templateUrl: "./helps-add.component.html",
-  styleUrls: ["./helps-add.component.css"]
+  selector: 'app-helps-add',
+  templateUrl: './helps-add.component.html',
+  styleUrls: ['./helps-add.component.css']
 })
 export class HelpsAddComponent implements OnInit {
   categories: HelpCategory[] = [];
   model: Help = null;
   submitted = false;
   currentUser: User;
-  latitude: number;
-  longitude: number;
   zoom: number;
   address: string;
   private geoCoder;
 
-  @ViewChild("search", { static: false })
+  @ViewChild('search', { static: false })
   public searchElementRef: ElementRef;
 
   constructor(
@@ -42,30 +40,82 @@ export class HelpsAddComponent implements OnInit {
     private ngZone: NgZone
   ) {
     this.as.currentUser.subscribe(x => {
-      console.log("user ==>");
-      console.log(x);
       this.currentUser = x;
       this.model = new Help();
       this.model.idCreator = this.currentUser.id;
       this.model.idType = 1;
-      // if ("geolocation" in navigator) {
-      //   navigator.geolocation.getCurrentPosition(position => {
-      //     this.model.latitude = position.coords.latitude;
-      //     this.model.longitude = position.coords.longitude;
-      //     this.zoom = 8;
-      //     this.getAddress(this.model.latitude, this.model.longitude);
-      //   });
-      //   // this.model.latitude = this.latitude;
-      //   // this.model.longitude = this.longitude;
-      //   // this.model.address = this.address;
-      //   console.log("model ==>");
-      //   console.log(this.model, "model");
-      //   console.log(this.model.latitude, "this.latitude");
-      // }
     });
     this.cs.getAll().subscribe(x => {
       this.categories = x;
     });
+  }
+
+  ngOnInit() {
+    this.mapsAPILoader.load().then(() => {
+      this.setCurrentLocation();
+      this.geoCoder = new google.maps.Geocoder();
+
+      const autocomplete = new google.maps.places.Autocomplete(
+        this.searchElementRef.nativeElement,
+        {
+          types: ['address']
+        }
+      );
+      autocomplete.addListener('place_changed', () => {
+        this.ngZone.run(() => {
+          // get the place result
+          const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+
+          // verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
+          }
+
+          // set latitude, longitude and zoom
+          this.model.latitude = place.geometry.location.lat();
+          this.model.longitude = place.geometry.location.lng();
+          this.zoom = 12;
+        });
+      });
+    });
+  }
+
+  // Get current location coordinates
+  private setCurrentLocation() {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.model.latitude = position.coords.latitude;
+        this.model.longitude = position.coords.longitude;
+        this.zoom = 8;
+        this.getAddress(this.model.latitude, this.model.longitude);
+      });
+    }
+  }
+
+  markerDragEnd($event: MouseEvent) {
+    this.model.latitude = $event.coords.lat;
+    this.model.longitude = $event.coords.lng;
+    this.getAddress(this.model.latitude, this.model.longitude);
+  }
+
+  getAddress(latitude, longitude) {
+    if (this.geoCoder) {
+    this.geoCoder.geocode(
+      { location: { lat: latitude, lng: longitude } },
+      (results, status) => {
+        if (status === 'OK') {
+          if (results[0]) {
+            this.zoom = 12;
+            this.model.address = results[0].formatted_address;
+          } else {
+            window.alert('No results found');
+          }
+        } else {
+          window.alert('Geocoder failed due to: ' + status);
+        }
+      }
+    );
+    }
   }
 
   onSubmit() {
@@ -81,74 +131,6 @@ export class HelpsAddComponent implements OnInit {
     // );
   }
 
-  ngOnInit() {
-    this.mapsAPILoader.load().then(() => {
-      this.setCurrentLocation();
-      this.geoCoder = new google.maps.Geocoder();
-
-      const autocomplete = new google.maps.places.Autocomplete(
-        this.searchElementRef.nativeElement,
-        {
-          types: ["address"]
-        }
-      );
-      autocomplete.addListener("place_changed", () => {
-        this.ngZone.run(() => {
-          // get the place result
-          const place: google.maps.places.PlaceResult = autocomplete.getPlace();
-
-          // verify result
-          if (place.geometry === undefined || place.geometry === null) {
-            return;
-          }
-
-          // set latitude, longitude and zoom
-          this.latitude = place.geometry.location.lat();
-          this.longitude = place.geometry.location.lng();
-          this.zoom = 12;
-        });
-      });
-    });
-  }
-
-  // Get current location coordinates
-  private setCurrentLocation() {
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(position => {
-        this.latitude = position.coords.latitude;
-        this.longitude = position.coords.longitude;
-        this.zoom = 8;
-        this.getAddress(this.latitude, this.longitude);
-      });
-    }
-  }
-
-  markerDragEnd($event: MouseEvent) {
-    console.log($event);
-    this.latitude = $event.coords.lat;
-    this.longitude = $event.coords.lng;
-    this.getAddress(this.latitude, this.longitude);
-  }
-
-  getAddress(latitude, longitude) {
-    this.geoCoder.geocode(
-      { location: { lat: latitude, lng: longitude } },
-      (results, status) => {
-        console.log(results);
-        console.log(status);
-        if (status === "OK") {
-          if (results[0]) {
-            this.zoom = 12;
-            this.address = results[0].formatted_address;
-          } else {
-            window.alert("No results found");
-          }
-        } else {
-          window.alert("Geocoder failed due to: " + status);
-        }
-      }
-    );
-  }
 
   // addHelps() {
   //   const payload = {

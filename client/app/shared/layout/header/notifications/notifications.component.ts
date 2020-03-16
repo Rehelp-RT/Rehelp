fa52954ff4e6d67d65f7b74b556abfdd31644f46
Danilo Paoletti <danilopaoletti91@gmail.com>
2020-03-16 18:45:51 +0100

    modifiche su dev prima del merge su notifications

---
+++ client/app/shared/layout/header/notifications/notifications.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Input  } from '@angular/core';
+import { User } from '@app/models';
+import { Router } from '@angular/router';
+import { NotificationService } from '@app/services';
+
+@Component({
+  selector: 'app-notifications',
+  templateUrl: './notifications.component.html',
+  styleUrls: ['./notifications.component.css']
+})
+export class NotificationsComponent implements OnInit {
+
+  @Input() currentUser: User;
+  notifications: Notification[] = [];
+  isOpen: boolean = false;
+
+
+  constructor(
+    private router: Router,
+    private ns: NotificationService) { }
+
+  ngOnInit() {
+    this.getNotifications();
+  }
+
+  
+  
+  getNotifications(): void {
+    this.ns.getByUser(this.currentUser.id)
+      .subscribe(x => {
+        this.notifications = x;
+      },
+        (err) => { 
+          console.log('errore :', err) 
+      });
+  }
+
+  checkNotification(n : number):void {
+    this.ns.checkNotification(n)
+    .subscribe(x => {
+      //
+    })
+  }
+}

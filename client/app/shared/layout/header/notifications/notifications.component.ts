e76b81059921e6cc42795baec1710de349319c07
Danilo Paoletti <danilopaoletti91@gmail.com>
2020-05-05 18:54:57 +0200

    edit notification

--- client/app/shared/layout/header/notifications/notifications.component.ts
+++ client/app/shared/layout/header/notifications/notifications.component.ts
@@ -16,7 +16,7 @@ export class NotificationsComponent implements OnInit {
   notifications: Notification[] = [];
   notificationsNumber: number;
   isOpen = false;
-  readonly VAPID_PUBLIC_KEY = "BMFfBA24EM7OSESBe6gRHhuKB1u2YHGUnL-wlEzKUh7gJ_Vnd9dKSwuw64TeMvfc5r1KNZc7Mkpk-92O51TmHTU";
+  //readonly VAPID_PUBLIC_KEY = "BMFfBA24EM7OSESBe6gRHhuKB1u2YHGUnL-wlEzKUh7gJ_Vnd9dKSwuw64TeMvfc5r1KNZc7Mkpk-92O51TmHTU";
 
 
   @HostListener('document:click', ['$event'])
@@ -29,33 +29,31 @@ export class NotificationsComponent implements OnInit {
   constructor(
     private router: Router,
     private notificationService: NotificationService,
-    private eRef: ElementRef ) { 
-      // if (swPush.isEnabled) {
-      //   swPush
-      //     .requestSubscription({
-      //       serverPublicKey: this.VAPID_PUBLIC_KEY,
-      //     })
-      //     .then(subscription => {
-      //       // send subscription to the server
-      //     })
-      //     .catch(console.error)
-      // }
-    }
+    private eRef: ElementRef) {
+    // if (swPush.isEnabled) {
+    //   swPush
+    //     .requestSubscription({
+    //       serverPublicKey: this.VAPID_PUBLIC_KEY,
+    //     })
+    //     .then(subscription => {
+    //       // send subscription to the server
+    //     })
+    //     .catch(console.error)
+    // }
+  }
 
   ngOnInit() {
     if (this.currentUser != null) {
-      interval(5000)
-      .pipe(
+      interval(3000)
+        .pipe(
           startWith(0),
           switchMap(() => this.notificationService.getByUser(this.currentUser.id))
-      )
-      .subscribe(res => {
+        )
+        .subscribe(res => {
           this.notifications = res;
-      });
+          this.notificationsNumber = res.length;
+        });
     }
-  console.log('notifications',this.notifications);
-  console.log('notificationsNumber',this.notificationsNumber);
-
   }
 
   navigate(id: number) {
@@ -78,7 +76,7 @@ export class NotificationsComponent implements OnInit {
 
   checkNotification(n: Notification) {
     this.isOpen = false;
-    this.notificationsNumber --;
+    this.notificationsNumber--;
     this.notificationService.checkNotification(n.id)
       .subscribe(x => {
         n.checked = true;

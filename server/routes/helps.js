eaa498d79a4374da51d0135f1b0ffd332ce8de2d
Danilo Paoletti <danilopaoletti91@gmail.com>
2020-05-05 18:10:33 +0200

    bug fix helps and about page

--- server/routes/helps.js
+++ server/routes/helps.js
@@ -6,7 +6,7 @@ const { Op, Sequelize } = require('sequelize');
 router.get('/', (req, res) => {
 
     //mt to km
-    var distance = req.query.distance * 1000;
+    var distance = req.query.distance !== undefined ? req.query.distance * 1000 : undefined;
     var latit = req.query.lat !== undefined ? parseFloat(req.query.lat) : undefined;
     var longit = req.query.long !== undefined ? parseFloat(req.query.long) : undefined;
 

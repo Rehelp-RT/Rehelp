bbb966c407fb549b87dfaf6e0c9ce03cf145690b
Michelangelo Marani <michelangelomarani88@gmail.com>
2020-05-08 18:05:42 +0200

    new seeder

--- server/seeders/20200203101705-populate-types.js
+++ server/seeders/20200203101705-populate-types.js
@@ -2,14 +2,15 @@
 module.exports = {
     up: (queryInterface, Sequelize) => {
         const date = new Date();
-        return queryInterface.bulkInsert('HelpTypes', [
+        return Promise.all([
+            queryInterface.bulkDelete('HelpTypes', null, {}),
+            queryInterface.bulkInsert('HelpTypes', [
             { id: 1, code: 'MEH', createdAt: date, updatedAt: date, name: 'Incontra e aiuta (Meet and Help)' },
             { id: 2, code: 'COH', createdAt: date, updatedAt: date, name: 'Aiuto collettivo (Collaborative Help)' },
             { id: 3, code: 'IMH', createdAt: date, updatedAt: date, name: 'Aiuto immediato (Immediate Help)' },
-            { id: 4, code: 'MEU', createdAt: date, updatedAt: date, name: 'Parliamone insieme (Meet Up)' },
-            { id: 5, code: 'MES', createdAt: date, updatedAt: date, name: 'Incontra e condividi (Meet and Share)' },
-            { id: 6, code: 'GAS', createdAt: date, updatedAt: date, name: 'Condividi orto (Garden Share)' }
-        ], {});
+            { id: 4, code: 'MES', createdAt: date, updatedAt: date, name: 'Incontra e condividi (Meet and Share)' }
+            ], {})
+        ]);
     },
 
     down: (queryInterface, Sequelize) => {

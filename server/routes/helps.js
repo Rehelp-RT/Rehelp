ff5ddb76e6c40b1b972c5c3f847eecbdb81945f0
Michelangelo Marani <michelangelomarani88@gmail.com>
2020-05-10 20:55:44 +0200

    list of helps based on types

--- server/routes/helps.js
+++ server/routes/helps.js
@@ -7,12 +7,18 @@ const moment = require('moment');
 router.get('/', (req, res) => {
 
     //mt to km
-    var distance = req.query.distance !== undefined ? req.query.distance * 1000 : undefined;
-    var latit = req.query.lat !== undefined ? parseFloat(req.query.lat) : undefined;
-    var longit = req.query.long !== undefined ? parseFloat(req.query.long) : undefined;
+    const type = req.query.type !== undefined ? req.query.type : undefined;
+    const distance = req.query.distance !== undefined ? req.query.distance * 1000 : undefined;
+    const latit = req.query.lat !== undefined ? parseFloat(req.query.lat) : undefined;
+    const longit = req.query.long !== undefined ? parseFloat(req.query.long) : undefined;
 
     // get parameters
+    console.log('---- query ----');
+    console.log('---------------');
     console.log('req.query', req.query);
+    console.log('---------------');
+    console.log('---------------');
+    const filterType = type === undefined ? {} : { code: type };
     var filters = [];
     if (req.query.excludeUserId !== undefined) {
         filters.push({
@@ -44,9 +50,6 @@ router.get('/', (req, res) => {
                 ]
         });
     }
-
-    console.log('filters', filters);
-    const filterType = req.query.type === undefined || req.query.type === null ? {} : { code: req.query.type };
 
     // query
     db.Help.findAll({

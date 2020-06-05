9a6da58e89a9b7fbb0f276cb8f52cf310440ce5a
Michelangelo Marani <michelangelomarani88@gmail.com>
2020-06-05 14:03:10 +0200

    bugfix notification dropdown message that is not displaying when there are no notifications

--- client/app/shared/layout/header/notifications/notifications.component.ts
+++ client/app/shared/layout/header/notifications/notifications.component.ts
@@ -7,88 +7,66 @@ import { startWith, switchMap } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
 @Component({
-  selector: 'app-notifications',
-  templateUrl: './notifications.component.html',
-  styleUrls: ['./notifications.component.scss']
+    selector: 'app-notifications',
+    templateUrl: './notifications.component.html',
+    styleUrls: ['./notifications.component.scss']
 })
 export class NotificationsComponent implements OnInit {
 
-  @Input() currentUser: User;
-  notifications: Notification[] = [];
-  notificationsNumber: number;
-  isOpen = false;
-  //readonly VAPID_PUBLIC_KEY = "BMFfBA24EM7OSESBe6gRHhuKB1u2YHGUnL-wlEzKUh7gJ_Vnd9dKSwuw64TeMvfc5r1KNZc7Mkpk-92O51TmHTU";
+    @Input() currentUser: User;
+    notifications: Notification[] = [];
+    isOpen = false;
+    //readonly VAPID_PUBLIC_KEY = "BMFfBA24EM7OSESBe6gRHhuKB1u2YHGUnL-wlEzKUh7gJ_Vnd9dKSwuw64TeMvfc5r1KNZc7Mkpk-92O51TmHTU";
 
 
-  @HostListener('document:click', ['$event'])
-  clickout(event) {
-    if (!this.eRef.nativeElement.contains(event.target)) {
-      this.isOpen = false;
+    @HostListener('document:click', ['$event'])
+    clickout(event) {
+        if (!this.eRef.nativeElement.contains(event.target)) {
+            this.isOpen = false;
+        }
     }
-  }
 
-  constructor(
-    private router: Router,
-    private notificationService: NotificationService,
-    private eRef: ElementRef) {
-    // if (swPush.isEnabled) {
-    //   swPush
-    //     .requestSubscription({
-    //       serverPublicKey: this.VAPID_PUBLIC_KEY,
-    //     })
-    //     .then(subscription => {
-    //       // send subscription to the server
-    //     })
-    //     .catch(console.error)
-    // }
-  }
+    constructor(
+        private router: Router,
+        private notificationService: NotificationService,
+        private eRef: ElementRef) { }
 
-  ngOnInit() {
-    setInterval(() => {
-      if (this.currentUser != null) {
-        this.callFuntionAtIntervals();
-      }
-    }, 120000);
+    ngOnInit() {
+        if (this.currentUser != null) {
+            setInterval(() => {
+                this.callFuntionAtIntervals();
+            }, 120000);
+        }
+    }
 
-    // if (this.currentUser != null) {
-    //   interval(3000)
-    //     .pipe(
-    //       startWith(0),
-    //       switchMap(() => this.notificationService.getByUser(this.currentUser.id))
-    //     )
-    //     .subscribe(res => {
-    //       this.notifications = res;
-    //       this.notificationsNumber = res.length;
-    //     },
-    //       (err) => {
-    //         console.error('errore :', err);
-    //       });
-    // }
-  }
-  callFuntionAtIntervals() {
-    this.notificationService.getByUser(this.currentUser.id)
-      .subscribe(res => {
-        this.notifications = res;
-        this.notificationsNumber = res.length;
-      },
-        (err) => {
-          console.error('errore :', err);
-        });
-  }
+    callFuntionAtIntervals() {
+        this.notificationService.getByUser(this.currentUser.id)
+            .subscribe(res => {
+                this.notifications = res;
+            }, (err) => {
+                console.error('errore :', err);
+            });
+    }
 
-  navigate(id: number) {
-    this.isOpen = false;
-    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
-    this.router.onSameUrlNavigation = 'reload';
-    this.router.navigate(['/helps/', id]);
-  }
+    navigate(id: number) {
+        this.isOpen = false;
+        this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+        this.router.onSameUrlNavigation = 'reload';
+        this.router.navigate(['/helps/', id]);
+    }
 
-  checkNotification(n: Notification) {
-    this.isOpen = false;
-    this.notificationsNumber--;
-    this.notificationService.checkNotification(n.id)
-      .subscribe(x => {
-        n.checked = true;
-      });
-  }
+    checkNotification(n: Notification) {
+        this.isOpen = false;
+        
+        this.notificationService.checkNotification(n.id)
+            .subscribe(x => {
+                n.checked = true;
+
+                // remove notification from array
+                var index = this.notifications.indexOf(n);
+                if (index > -1) {
+                    this.notifications.splice(index, 1);
+                }
+            });
+    }
 }

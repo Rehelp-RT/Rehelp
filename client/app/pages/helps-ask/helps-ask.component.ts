673b20efd3e26eae66dbed671bbe6d5cec943cef
Danilo Paoletti <danilopaoletti91@gmail.com>
2020-06-17 01:26:44 +0200

    modifica helpask component

--- client/app/pages/helps-ask/helps-ask.component.ts
+++ client/app/pages/helps-ask/helps-ask.component.ts
@@ -63,8 +63,8 @@ export class HelpsAskComponent implements OnInit {
     ngOnInit() {
         const type =
             this.activeRouter.snapshot.params.type
-            ? this.activeRouter.snapshot.params.type
-            : 'MEH';
+                ? this.activeRouter.snapshot.params.type
+                : 'MEH';
 
         this.as.currentUser.subscribe(x => {
             this.currentUser = x;
@@ -72,16 +72,6 @@ export class HelpsAskComponent implements OnInit {
             this.model.idCreator = this.currentUser.id;
             this.model.idType = 1;
             this.model.isOffer = true;
-        });
-
-        const id = this.activeRouter.snapshot.params.id;
-        this.userService.getById(id).subscribe(x => {
-            this.userToAsk = x;
-            this.userService.getCategories(this.userToAsk.id, type).subscribe(y => {
-                if (y.categories && y.categories.length > 0) {
-                    this.categories = y.categories;
-                }
-            });
         });
 
         // get type
@@ -93,137 +83,147 @@ export class HelpsAskComponent implements OnInit {
         // init image
         this.initImage();
 
-      }
+    }
 
-      toggleImgUploader() {
-          this.imageUploaded = !this.imageUploaded;
-      }
+    toggleImgUploader() {
+        this.imageUploaded = !this.imageUploaded;
+    }
 
-      // Delete an uploaded image
-      // Requires setting 'Return delete token' to 'Yes' in your upload preset configuration
-      // See also https://support.cloudinary.com/hc/en-us/articles/202521132-How-to-delete-an-image-from-the-client-side-
-      deleteImage = function(data: any, index: number) {
-          const url = `https://api.cloudinary.com/v1_1/${
+    // Delete an uploaded image
+    // Requires setting 'Return delete token' to 'Yes' in your upload preset configuration
+    // See also https://support.cloudinary.com/hc/en-us/articles/202521132-How-to-delete-an-image-from-the-client-side-
+    deleteImage = function (data: any, index: number) {
+        const url = `https://api.cloudinary.com/v1_1/${
             this.cloudinary.config().cloud_name
             }/delete_by_token`;
-          console.log(url, 'url');
-          const headers = [{
-              name: 'X-Requested-With',
-              value: 'XMLHttpRequest'
-          }];
-          const options = { headers };
-          console.log(data, 'data');
-          const body = {
-              token: data.delete_token
-          };
-          console.log(body, 'body');
-          this.http.post(url, body, options).subscribe(response => {
-              console.log(`Deleted image - ${data.public_id} ${response.result}`);
-              // Remove deleted item for responses
-              this.responses.splice(index, 1);
-          });
-          this.imageUploaded = !this.imageUploaded;
-      };
+        console.log(url, 'url');
+        const headers = [{
+            name: 'X-Requested-With',
+            value: 'XMLHttpRequest'
+        }];
+        const options = { headers };
+        console.log(data, 'data');
+        const body = {
+            token: data.delete_token
+        };
+        console.log(body, 'body');
+        this.http.post(url, body, options).subscribe(response => {
+            console.log(`Deleted image - ${data.public_id} ${response.result}`);
+            // Remove deleted item for responses
+            this.responses.splice(index, 1);
+        });
+        this.imageUploaded = !this.imageUploaded;
+    };
 
-      fileOverBase(e: any): void {
-          this.hasBaseDropZoneOver = e;
-      }
+    fileOverBase(e: any): void {
+        this.hasBaseDropZoneOver = e;
+    }
 
-      getFileProperties(fileProperties: any) {
-          // Transforms Javascript Object to an iterable to be used by *ngFor
-          if (!fileProperties) {
-              return null;
-          }
-          return Object.keys(fileProperties).map(key => ({
-              key,
-              value: fileProperties[key]
-          }));
-      }
+    getFileProperties(fileProperties: any) {
+        // Transforms Javascript Object to an iterable to be used by *ngFor
+        if (!fileProperties) {
+            return null;
+        }
+        return Object.keys(fileProperties).map(key => ({
+            key,
+            value: fileProperties[key]
+        }));
+    }
 
-      // Get current location coordinates
-      private setCurrentLocation() {
-          if ('geolocation' in navigator) {
-              navigator.geolocation.getCurrentPosition(position => {
-                  this.model.latitude = position.coords.latitude;
-                  this.model.longitude = position.coords.longitude;
-                  this.zoom = 8;
-                  this.getAddress(this.model.latitude, this.model.longitude);
-              });
-          }
-      }
+    // Get current location coordinates
+    private setCurrentLocation() {
+        if ('geolocation' in navigator) {
+            navigator.geolocation.getCurrentPosition(position => {
+                this.model.latitude = position.coords.latitude;
+                this.model.longitude = position.coords.longitude;
+                this.zoom = 8;
+                this.getAddress(this.model.latitude, this.model.longitude);
+            });
+        }
+    }
 
-      markerDragEnd($event: MouseEvent) {
-          this.model.latitude = $event.coords.lat;
-          this.model.longitude = $event.coords.lng;
-          this.getAddress(this.model.latitude, this.model.longitude);
-      }
+    markerDragEnd($event: MouseEvent) {
+        this.model.latitude = $event.coords.lat;
+        this.model.longitude = $event.coords.lng;
+        this.getAddress(this.model.latitude, this.model.longitude);
+    }
 
-      getAddress(latitude, longitude) {
-          if (this.geoCoder) {
-              this.geoCoder.geocode({ 
-                  location: { lat: latitude, lng: longitude } 
-              },
-              (results, status) => {
-                  if (status === 'OK') {
-                      if (results[0]) {
-                          this.zoom = 12;
-                          this.model.address = results[0].formatted_address;
-                          this.lastAddress = results[0].formatted_address;
-                      } else {
-                          window.alert('No results found');
-                      }
-                  } else {
-                      window.alert('Geocoder failed due to: ' + status);
-                  }
-              });
-          }
-      }
+    getAddress(latitude, longitude) {
+        if (this.geoCoder) {
+            this.geoCoder.geocode({
+                location: { lat: latitude, lng: longitude }
+            },
+                (results, status) => {
+                    if (status === 'OK') {
+                        if (results[0]) {
+                            this.zoom = 12;
+                            this.model.address = results[0].formatted_address;
+                            this.lastAddress = results[0].formatted_address;
+                        } else {
+                            window.alert('No results found');
+                        }
+                    } else {
+                        window.alert('Geocoder failed due to: ' + status);
+                    }
+                });
+        }
+    }
 
-      initTypes(type) {
-          this.ts.getByCode(type).subscribe(x => {
-              this.type = x;
-              this.model.idType = x.id;
+    initTypes(type) {
+        this.ts.getByCode(type).subscribe(x => {
+            this.type = x;
+            this.model.idType = x.id;
 
-              if (this.type.code == 'IMH') {
-                  this.model.halfhourValidity = 1;
-              }
-          });
-      }
+            if (this.type.code == 'IMH') {
+                this.model.halfhourValidity = 1;
+            }
+            const id = this.activeRouter.snapshot.params.id;
+            this.userService.getById(id).subscribe(y => {
+                this.userToAsk = y;
+                console.log('this.type.code',this.type.code)
+                this.userService.getCategories(this.userToAsk.id, x.id).subscribe(y => {
+                    if (y.categories && y.categories.length > 0) {
+                        this.categories = y.categories;
+                    }
+                });
+            });
+        });
+    }
 
-      initMaps() {
-          this.mapsAPILoader.load().then(() => {
-              this.setCurrentLocation();
-              this.geoCoder = new google.maps.Geocoder();
+    initMaps() {
+        this.mapsAPILoader.load().then(() => {
+            this.setCurrentLocation();
+            this.geoCoder = new google.maps.Geocoder();
 
-              const autocomplete = new google.maps.places.Autocomplete(
+            const autocomplete = new google.maps.places.Autocomplete(
                 this.searchElementRef.nativeElement,
                 {
-                  types: ['address']
+                    types: ['address']
                 }
-              );
-              autocomplete.addListener('place_changed', () => {
+            );
+            autocomplete.addListener('place_changed', () => {
                 this.ngZone.run(() => {
-                  // get the place result
-                  const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+                    // get the place result
+                    const place: google.maps.places.PlaceResult = autocomplete.getPlace();
 
-                  // verify result
-                  if (place.geometry === undefined || place.geometry === null) {
-                    return;
-                  }
+                    // verify result
+                    if (place.geometry === undefined || place.geometry === null) {
+                        return;
+                    }
 
-                  // set latitude, longitude and zoom
-                  this.model.latitude = place.geometry.location.lat();
-                  this.model.longitude = place.geometry.location.lng();
-                  this.zoom = 12;
-                  this.getAddress(this.model.latitude, this.model.longitude);
+                    // set latitude, longitude and zoom
+                    this.model.latitude = place.geometry.location.lat();
+                    this.model.longitude = place.geometry.location.lng();
+                    this.zoom = 12;
+                    this.getAddress(this.model.latitude, this.model.longitude);
                 });
-              });
-          });
-      }
+            });
+        });
+    }
 
-      initImage() {
-          // init image - create the file uploader, wire it to upload to your account
-          const uploaderOptions: FileUploaderOptions = {
+    initImage() {
+        // init image - create the file uploader, wire it to upload to your account
+        const uploaderOptions: FileUploaderOptions = {
             url: `https://api.cloudinary.com/v1_1/${this.cloudinary.config().cloud_name}/upload`,
             // Upload files automatically upon addition to upload queue
             autoUpload: true,
@@ -264,26 +264,26 @@ export class HelpsAskComponent implements OnInit {
             // as part of the XHR request to upload the files.
             // Running in a custom zone forces change detection
             this.ngZone.run(() => {
-              // Update an existing entry if it's upload hasn't completed yet
+                // Update an existing entry if it's upload hasn't completed yet
 
-              // Find the id of an existing item
-              const existingId = this.responses.reduce((prev, current, index) => {
-                  if (current.file.name === fileItem.file.name && !current.status) {
-                    return index;
-                  }
-                  return prev;
-              }, -1);
-              if (existingId > -1) {
-                  // Update existing item with new data
-                  this.responses[existingId] = Object.assign(
-                    this.responses[existingId],
-                    fileItem
-                  );
-              } else {
-                  // Create new response
-                  this.responses.push(fileItem);
-              }
-              this.imageUploaded = true;
+                // Find the id of an existing item
+                const existingId = this.responses.reduce((prev, current, index) => {
+                    if (current.file.name === fileItem.file.name && !current.status) {
+                        return index;
+                    }
+                    return prev;
+                }, -1);
+                if (existingId > -1) {
+                    // Update existing item with new data
+                    this.responses[existingId] = Object.assign(
+                        this.responses[existingId],
+                        fileItem
+                    );
+                } else {
+                    // Create new response
+                    this.responses.push(fileItem);
+                }
+                this.imageUploaded = true;
             });
         };
         // Update model on completion of uploading a file
@@ -305,60 +305,60 @@ export class HelpsAskComponent implements OnInit {
                 progress,
                 data: {}
             });
-      }
+    }
 
-      onSubmit() {
-          this.submitted = true;
-          // image
-          const i = this.responses.length - 1;
-          const image = this.responses[i];
-          this.model.image = image === undefined ? null : image.data.public_id;
+    onSubmit() {
+        this.submitted = true;
+        // image
+        const i = this.responses.length - 1;
+        const image = this.responses[i];
+        this.model.image = image === undefined ? null : image.data.public_id;
 
-          this.model.address = this.lastAddress;
-          // category
-          this.model.idCategory = this.idCat;
+        this.model.address = this.lastAddress;
+        // category
+        this.model.idCategory = this.idCat;
 
-          this.addHelp();
-      }
+        this.addHelp();
+    }
 
-      addHelp() {
-          // crea l'help
-          this.hs.addHelp(this.model).subscribe(x => {
-              this.response = new HelpResponse();
-              this.response.responder = this.userToAsk;
-              this.response.idResponder = this.userToAsk.id;
-              this.response.help = x;
-              this.response.idHelp = x.id;
+    addHelp() {
+        // crea l'help
+        this.hs.addHelp(this.model).subscribe(x => {
+            this.response = new HelpResponse();
+            this.response.responder = this.userToAsk;
+            this.response.idResponder = this.userToAsk.id;
+            this.response.help = x;
+            this.response.idHelp = x.id;
 
-              this.addResponse();
-          },
-          err => {
-              console.log('errore addHelp', err);
-          });
-      }
+            this.addResponse();
+        },
+            err => {
+                console.log('errore addHelp', err);
+            });
+    }
 
-      addResponse() {
+    addResponse() {
         // crea la risposta
         this.responseService.addResponse(this.response).subscribe(
-          res => {
-            this.router.navigate(['/helps/', this.response.help.id]);
-            this.acceptResponse();
-          },
-          err => {
-            console.log('errore addResponse', err);
-          }
+            res => {
+                this.router.navigate(['/helps/', this.response.help.id]);
+                this.acceptResponse();
+            },
+            err => {
+                console.log('errore addResponse', err);
+            }
         );
-      }
+    }
 
-      acceptResponse() {
-          // accetta la risposta
-          this.responseService.acceptResponse(this.response).subscribe(x => {
-              this.router.navigate(['/helps/', this.response.help.id]);
-          },
-          err => {
-              console.log('errore acceptResponse', err);
-          });
-      }
+    acceptResponse() {
+        // accetta la risposta
+        this.responseService.acceptResponse(this.response).subscribe(x => {
+            this.router.navigate(['/helps/', this.response.help.id]);
+        },
+            err => {
+                console.log('errore acceptResponse', err);
+            });
+    }
 
     // async addHelpPromise(): Promise<any> {
     //   return new Promise((resolve) => {

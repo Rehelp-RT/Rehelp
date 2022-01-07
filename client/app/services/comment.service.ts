fd0fb6634338d7954f19188b29189163b819d05a
Costello1 <pietro.angelici@studenti.unicam.it>
2022-01-07 15:04:22 +0100

    fixed users

---
+++ client/app/services/comment.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse
+} from '@angular/common/http';
+import { environment } from '@environments/environment';
+import { Help } from '@app/models';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CommentService {
+
+  private api = environment.apiUrl;
+    private httpOptions = {
+        headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+    };
+
+  constructor(private http: HttpClient) { }
+
+  getAll(
+    idPost = null, 
+    idCreator = null, 
+    idHelp = null, 
+    message = null, ) {
+    let params = '';
+    params += idPost != null ? 'idPost=' + idPost + '&' : '';
+    params += idHelp != null ? 'idHelp=' + idHelp + '&' : '';
+    params += message != null ? 'message=' + message + '&' : '';
+    params += idCreator != null ? 'idCreator=' + idCreator + '&' : '';
+    if (params !== '') {
+      params = '?' + params;
+    }
+    // console.log('params', params);
+
+    return this.http.get<Help[]>(`${this.api}/helps${params}`);
+}
+
+
+}

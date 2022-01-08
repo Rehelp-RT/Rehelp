0b54b311fa9eed0fa2962bb5f10d799240ba94b6
Costello1 <pietro.angelici@studenti.unicam.it>
2022-01-08 17:27:50 +0100

    bacheca implementation

--- client/app/pages/bacheca/bacheca.component.ts
+++ client/app/pages/bacheca/bacheca.component.ts
@@ -1,4 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  AuthenticationService,
+  DistanceService,
+  CommentService,
+  UserService,
+  TypeService,
+  CategoryService,
+} from "@app/services";
+import {
+  Help,
+  User,
+  HelpType,
+  HelpCategory,
+  HelpResponse,
+  Comment,
+  Post,
+} from "@app/models";
+import { ActivatedRoute } from "@angular/router";
+import { ForumPostService } from "@app/services/forum-post.service";
 
 @Component({
   selector: 'app-bacheca',
@@ -7,9 +26,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BachecaComponent implements OnInit {
 
-  constructor() { }
+  currentUser: User = null;
+  posts: Post[] = [];
+
+  constructor(
+    private fps: ForumPostService,
+    private as: AuthenticationService,
+
+    private actRoute: ActivatedRoute
+  ) {
+    this.getCurrentUser();
+  }
 
   ngOnInit(): void {
+    /* const idPost = this.actRoute.snapshot.params.id;
+    this.cs.getAll<Comment[]>( idPost, null, null, null).subscribe(res => {
+      this.comments = res;
+     console.log(this.comments)
+  });*/
+
+    this.fps.getAll(null, null).subscribe((res) => {
+      this.posts = res;
+      console.log(this.posts);
+    });
+  }
+
+  post(): void {
+    this.fps.getAll;
+  }
+
+  getCurrentUser(): void {
+    this.as.getCurrentUser().subscribe((x) => {
+      this.currentUser = x;
+    });
   }
 
 }
+
